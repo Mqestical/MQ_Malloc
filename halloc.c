4bfce69601a12cee3eac469a83350744266bc6dc
Mqestical <145111762+Mqestical@users.noreply.github.com>
2024-05-10 06:05:36 -0700

    thanks qwinci dawg
    made sure the integrity is valid, everything checked.

--- halloc.c
+++ halloc.c
@@ -1,3 +1,16 @@
+
+/*  |===========================================================
+    |                                                          |
+    |                                                          |
+    | Copyright (c) Muhammad Alhijab (Kingdom of Saudi Arabia) |
+    |                                                          |
+    |                                                          |
+    |==========================================================|
+*/
+
+
+
+
 #include "halloc.h"
 #include <stdlib.h>
 #include <sys/mman.h>
@@ -5,11 +18,21 @@
 #include <unistd.h>
 #include <stdatomic.h>
 
+/*ANYTHING THAT IS ASSOSCIATED WITH ROUNDING UP / FINDING ADDRESSES / CALCULATIONS IS FROM AN EXTERNAL SOURCE.*/
+
+
 
 
 atomic_flag allocator_spinlock = ATOMIC_FLAG_INIT;
 struct PAGE_INFORMATION *page_info = NULL;
-void* next_available_address = NULL; // Global variable to track next available memory address
+void* next_available_address = NULL;
+
+
+
+
+
+struct MemoryBlock allocation_table[MAX_BLOCKS];
+int num_allocated_blocks = 0;
 
 void acquire_spinlock() {
     while(atomic_flag_test_and_set(&allocator_spinlock)) {
@@ -21,65 +44,96 @@ void release_spinlock() {
     atomic_flag_clear(&allocator_spinlock);
 }
 
-// MEM_INIT: Can only be called once, it is the INTERNAL OF HALLOC.
+
+// from an external source
+void add_to_allocation_table(void* address, size_t size) {
+    if (num_allocated_blocks < MAX_BLOCKS) {
+        allocation_table[num_allocated_blocks].address = address;
+        allocation_table[num_allocated_blocks].size = size;
+        num_allocated_blocks++;
+    } else {
+        fprintf(stderr, "fatal error: Maximum number of allocated blocks reached!\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+// from an external source
+void remove_from_allocation_table(void* address) {
+    for (int i = 0; i < num_allocated_blocks; ++i) {
+        if (allocation_table[i].address == address) {
+            
+            for (int j = i; j < num_allocated_blocks - 1; ++j) {
+                allocation_table[j] = allocation_table[j + 1];
+            }
+            num_allocated_blocks--;
+            return;
+        }
+    }
+}
+
 int MEM_init(int64_t sizeofRegion) {
-    if (sizeofRegion <= 0x0) {
+    if (sizeofRegion <= 0) {
         fprintf(stderr, "\nfatal error: SizeOfRegion cannot be less than or equal to 0!\n\n");
-        exit(FAILURE);
+        exit(EXIT_FAILURE);
     }
 
     size_t page_size = sysconf(_SC_PAGESIZE);
-    int num_pages = (sizeofRegion + page_size - 0x1) / page_size;
+    int num_pages = (sizeofRegion + page_size - 1) / page_size;
     size_t info_size = sizeof(struct PAGE_INFORMATION) + sizeof(char) * num_pages * page_size;
-    page_info = mmap(NULL, info_size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -0x1, 0x0);
+    page_info = mmap(NULL, info_size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
 
     if (page_info == MAP_FAILED) {
         perror("mmap");
-        exit(-1);
+        exit(EXIT_FAILURE);
     }
 
     page_info->pages = num_pages;
     page_info->page_size = page_size;
     next_available_address = page_info->page_data;
-    printf("Initialized with %d pages of size %d\n", page_info->pages, page_info->page_size);
-    next_available_address = page_info->page_data; // Set next_available_address to point to the start of the allocated memory region
-    printf("Next available address: %p\n", next_available_address); // Debug print
-    return 0x0;
+    printf("Initialized with %d pages of size %zu\n", page_info->pages, page_info->page_size);
+    next_available_address = page_info->page_data;
+    printf("Next available address: %p\n", next_available_address);
+    return 0;
 }
 
 void* halloc(int64_t size) {
     acquire_spinlock();
 
-    if (size <= 0x0) {
+    if (size <= 0) {
         fprintf(stderr, "error: size must be a positive number.\n");
-        exit(FAILURE);
+        exit(EXIT_FAILURE);
     }
 
     size_t available_space = (page_info->pages * page_info->page_size) - ((size_t)next_available_address - (size_t)page_info);
 
-    if (size + 2 * TAG_SIZE > available_space) { // Include space for tags
-        fprintf(stderr, "error: Not enough memory available.\n");
-        exit(FAILURE);
+    if (size + 2 * TAG_SIZE > available_space) { 
+        fprintf(stderr, "fatal error: not enough memory available.\n");
+        exit(EXIT_FAILURE);
     }
 
     void* allocated_memory = next_available_address;
 
-    
-    struct BlockTag* start_tag = (struct BlockTag*)allocated_memory;
+    size_t* size_ptr = (size_t*)allocated_memory;
+    *size_ptr = size;
+
+    struct BlockTag* start_tag = (struct BlockTag*)next_available_address;
     start_tag->tag_start = 'A';
 
-    
-    struct BlockTag* end_tag = (struct BlockTag*)((char*)allocated_memory + size + TAG_SIZE);
+    next_available_address = (void*)((char*)next_available_address + sizeof(size_t) + TAG_SIZE);
+
+    struct BlockTag* end_tag = (struct BlockTag*)((char*)allocated_memory + sizeof(size_t) + size + TAG_SIZE);
     end_tag->tag_end = 'B';
 
-    
-    next_available_address = (void*)((char*)next_available_address + size + 2 * TAG_SIZE);
+    next_available_address = (void*)((char*)next_available_address + size + TAG_SIZE);
+
+    /*align the next available address to 0x7&~0x7 (8-byte aligned addr)*/
+    next_available_address = (void*)(((size_t)next_available_address + 7) & ~7);
 
     
-    next_available_address = (void*)(((size_t)next_available_address + 0x7) & ~0x7);
+    add_to_allocation_table(allocated_memory, size);
 
     release_spinlock();
-    return (char*)allocated_memory + TAG_SIZE; 
+    return (char*)allocated_memory + sizeof(size_t);
 }
 
 void free_internal(struct PAGE_INFORMATION* header) {
@@ -98,31 +152,52 @@ int hfree(void* ptr) {
 
     if (ptr != NULL) {
         
-        struct BlockTag* start_tag = (struct BlockTag*)((char*)ptr - TAG_SIZE);
+        uintptr_t page_start = ((uintptr_t)ptr & ~(page_info->page_size - 1));
+        struct PAGE_INFORMATION* header = (struct PAGE_INFORMATION*)((uintptr_t)ptr - sizeof(struct PAGE_INFORMATION));
 
-        
-        if (start_tag->tag_start != 'A') {
-            fprintf(stderr, "error: Invalid start tag detected!\n");
-            exit(FAILURE);
+
+    
+        uintptr_t block_start = (uintptr_t)header->page_data;
+        uintptr_t block_end = block_start + header->pages * header->page_size;
+        while (block_start < block_end) {
+            
+            if ((uintptr_t)ptr >= block_start && (uintptr_t)ptr < block_start + header->page_size) {
+                
+                struct BlockTag* start_tag = (struct BlockTag*)(block_start + sizeof(size_t));
+
+                
+                if (start_tag->tag_start != 'A') {
+                    fprintf(stderr, "\n\nfatal error: Heap Corrupted! (FATAL_ERROR_START_TAG_NOT_AVAILABLE)\n\n");
+                    exit(EXIT_FAILURE);
+                }
+
+                
+                struct BlockTag* end_tag = (struct BlockTag*)(block_start + sizeof(size_t) + *(size_t*)block_start + TAG_SIZE);
+
+                
+                if (end_tag->tag_end != 'B') {
+                    fprintf(stderr, "fatal error: Heap Corrupted! (FATAL_ERROR_END_TAG_NOT_AVAILABLE)");
+                    exit(EXIT_FAILURE);
+                }
+
+                
+                remove_from_allocation_table((void*)block_start);
+
+                
+                free_internal(header);
+
+                
+                break;
+            }
+
+            
+            block_start += header->page_size;
         }
-
-        
-        struct BlockTag* end_tag = (struct BlockTag*)((char*)ptr + sizeof(ptr));
-
-        
-        if (end_tag->tag_end != 'B') {
-            fprintf(stderr, "error: Invalid end tag detected!\n");
-            exit(FAILURE);
-        }
-
-        
-        struct PAGE_INFORMATION* header = (struct PAGE_INFORMATION*)((size_t)ptr - sizeof(struct PAGE_INFORMATION));
-        free_internal(header);
     } else {
         fprintf(stderr, "fatal error: pointer is null!\n");
-        exit(FAILURE);
+        exit(EXIT_FAILURE);
     }
 
     release_spinlock();
-    return 0x0;
+    return 0;
 }
--- halloc.h
+++ halloc.h
@@ -3,6 +3,7 @@
 #define FAILURE -0x1
 #define SUCCESS 0x0
 #define TAG_SIZE sizeof(struct BlockTag)
+#define MAX_BLOCKS 1000
 
 /*INPUT_INFORMATION:*/
 /*UTF-8 LF C { POSIX : LINUX } */
@@ -18,8 +19,12 @@ struct BlockTag {
     char tag_end;   // tag at the end of the block
 };
 
+struct MemoryBlock {
+    void* address;
+    size_t size;
+};
 
-extern int MEM_init(int64_t size);
+extern int MEM_init(int64_t sizeofRegion);
 extern void* halloc(int64_t size) __THROW __wur;
 extern void free_internal(struct PAGE_INFORMATION* header);
 
